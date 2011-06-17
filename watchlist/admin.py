b3ea4f2e0bebc46c396fc105131557c55588e5ab
Sam Hartsfield <samh.public@gmail.com>
2011-06-16 20:33:30 -0400

    Tweaked admin list view

--- watchlist/admin.py
+++ watchlist/admin.py
@@ -6,6 +6,13 @@ class ShowAdmin(admin.ModelAdmin):
     list_editable = ('watch_state', 'progress')
     list_filter = ('type', 'watch_state', 'production_type')
     
+    date_hierarchy = 'timestamp_modified'
+    
+    # The submit button for the actions seems to take precedence when pressing
+    # "Enter" to save the fields editable in the list.
+    actions_on_top = False
+    actions_on_bottom = False
+    
     #radio_fields = {"watch_state": admin.HORIZONTAL}
     
     search_fields = ['title']
